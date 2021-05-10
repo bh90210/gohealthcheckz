424c5254c8985c31a5ba0ade84d8240153f1d214
bh90210 <ktc@protonmail.com>
2021-05-10 05:21:30 +0300

    feat: go submodule

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module github.com/bh90210/healthz
 
 go 1.15
-
-require (
-	github.com/jepanetwork/grpc v0.0.0-20201026110026-560643cbfd8c // indirect
-	github.com/johnsiilver/getcert v0.0.0-20201005022436-57fe9706afac // indirect
-)
--- healthz.go
+++ healthz.go
@@ -2,16 +2,7 @@
 package healthz
 
 import (
-	"context"
-	"crypto/x509"
-	"log"
 	"net/http"
-	"time"
-
-	"github.com/jepanetwork/grpc/proto"
-	"github.com/johnsiilver/getcert"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 )
 
 type Service int
@@ -82,34 +73,4 @@ func live(req chan State, res chan bool) http.HandlerFunc {
 			w.WriteHeader(http.StatusOK)
 		}
 	}
-}
-
-func GRPCLiveCheck() int {
-	_, xCert, err := getcert.FromTLSServer("service.jepa.network:50051", true)
-	roots := x509.NewCertPool()
-	roots.AddCert(xCert[0])
-	creds := credentials.NewClientTLSFromCert(roots, "")
-
-	opts := []grpc.DialOption{
-		grpc.WithTransportCredentials(creds),
-	}
-
-	// Set up a connection to the server.
-	conn, err := grpc.Dial("service.jepa.network:50051", opts...)
-	if err != nil {
-		log.Println("gRPC connection error: ", err)
-	}
-	defer conn.Close()
-
-	client := proto.NewHealthClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	check, err := client.Check(ctx, &proto.HealthCheckRequest{})
-	if err != nil {
-		log.Println("Client error: ", err)
-	}
-	ok := check.GetStatus()
-	return int(ok)
 }
