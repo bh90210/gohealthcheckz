fb4fae31addd4ad6820492a5693435892acfbec9
bh90210 <ktc@protonmail.com>
2021-05-12 09:52:07 +0300

    fix: terminating, example, configs

--- examples/http/main.go
+++ examples/http/main.go
@@ -1,48 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/bh90210/healthz"
 )
 
 func init() {
-	log.Println("init")
 	go func() {
-		log.Println("go")
-		err := healthz.Start()
-		if err != nil {
+		if err := healthz.Start(); err != nil {
 			log.Fatalln(err)
 		}
 	}()
-	log.Println("finish init")
 }
 
 func main() {
-	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
-
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
 	go func() {
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		done <- true
+		log.Println("ready")
+		healthz.Ready()
+		time.Sleep(time.Second * 60)
+		log.Println("not ready")
+		healthz.NotReady()
 	}()
 
-	log.Println("ready")
-	healthz.Ready()
-	time.Sleep(time.Second * 60)
-	log.Println("not ready")
-	healthz.NotReady()
-
-	fmt.Println("awaiting signal")
-	<-done
-	fmt.Println("exiting")
+	if term := healthz.Terminating(); term == true {
+		// do something
+	}
 }
--- healthz.go
+++ healthz.go
@@ -3,6 +3,9 @@ package healthz
 
 import (
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type state int
@@ -21,22 +24,26 @@ func Start() error {
 	return http.ListenAndServe(":6080", nil)
 }
 
+// Ready .
 func Ready() {
 	s = ready
 }
 
+// NotReady .
 func NotReady() {
 	s = notready
 }
 
-func readiness() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if s == ready {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		w.WriteHeader(http.StatusServiceUnavailable)
-	}
+// Terminating .
+func Terminating() bool {
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		done <- true
+	}()
+	return <-done
 }
 
 func liveness() http.HandlerFunc {
@@ -49,8 +56,12 @@ func liveness() http.HandlerFunc {
 	}
 }
 
-// startup
-
-func terminating() {
-
+func readiness() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if s == ready {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		w.WriteHeader(http.StatusServiceUnavailable)
+	}
 }
